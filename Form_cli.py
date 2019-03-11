aeebf268b99a4666efc63491e4417d70e153e6ba
zhengjie607 <39082181+zhengjie607@users.noreply.github.com>
2019-03-11 17:31:23 +0800

    Add files via upload

---
+++ Form_cli.py
@@ -0,0 +1,69 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from gl_widget import GLWidget
+from CLI_Reader import CLI
+class Ui_Read_cli(QtWidgets.QWidget):
+    def __init__(self):
+        super(Ui_Read_cli,self).__init__()
+        
+        self.setObjectName("Form")
+        self.resize(950, 700)
+        self.setWindowTitle(QtCore.QCoreApplication.translate("Form", "Form"))
+        
+        self.widget = GLWidget(self)
+        self.widget.setGeometry(QtCore.QRect(0, 0, 650, 650))
+        self.widget.setObjectName("widget")
+        self.widget.setWindowTitle(QtCore.QCoreApplication.translate("Read_cli", "Form"))
+        
+        self.verticalSlider = QtWidgets.QSlider(self)
+        self.verticalSlider.setGeometry(QtCore.QRect(680, 40, 19, 601))
+        self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
+        self.verticalSlider.setObjectName("verticalSlider")
+        
+        self.pushButton = QtWidgets.QPushButton(self)
+        self.pushButton.setGeometry(QtCore.QRect(750, 40, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setText(QtCore.QCoreApplication.translate("Read_cli", "导入"))
+        
+        self.pushButton_2 = QtWidgets.QPushButton(self)
+        self.pushButton_2.setGeometry(QtCore.QRect(750, 80, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setText(QtCore.QCoreApplication.translate("Read_cli", "删除"))
+        
+        self.listWidget = QtWidgets.QListWidget(self)
+        self.listWidget.setGeometry(QtCore.QRect(720, 120, 190, 192))
+        self.listWidget.setObjectName("listWidget")
+        
+        self.lineEdit = QtWidgets.QLineEdit(self)
+        self.lineEdit.setGeometry(QtCore.QRect(670, 10, 41, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setText('0')
+        
+        self.pushButton.clicked.connect(self.inputfile)
+        self.pushButton_2.clicked.connect(self.delete)
+        self.verticalSlider.valueChanged['int'].connect(self.redraw)
+    def inputfile(self):
+        dialog=QtWidgets.QFileDialog()
+        f=dialog.getOpenFileName(self,'Open File','./喷杆cli','CLI File (*.cli)')
+        file=CLI(f[0])
+        self.widget.data.append(file.UsePoint)
+        self.verticalSlider.setMaximum(file.layer)
+        self.listWidget.addItem((f[0].split('/'))[-1].split('.')[0])
+    def redraw(self):
+        self.lineEdit.setText(str(self.verticalSlider.value()))
+        self.widget.layerindex=self.verticalSlider.value()
+        self.widget.update()
+    def delete(self):
+        self.listWidget.clear()
+app=QtWidgets.QApplication(sys.argv)
+mywindow=Ui_Read_cli()
+mywindow.show()
+app.exec_()
+sys.exit()
+
---
+++ gl_widget.py
@@ -0,0 +1,111 @@
+from PyQt5.QtWidgets import QOpenGLWidget
+from PyQt5 import QtCore, QtGui, QtWidgets
+from OpenGL.GL import *
+from OpenGL.arrays import vbo
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from Camera import camera
+from Vector3f import *
+from numpy import array
+import numpy
+class GLWidget(QOpenGLWidget):
+    def __init__(self, *args, **kwargs):
+        super(GLWidget, self).__init__(*args, **kwargs)
+        self.setGeometry(QtCore.QRect(10, 10,700, 700))#窗口位置和大小
+        self.isMove=False
+        self.isRotate=False
+        self.data=[]
+        self.layerindex=0
+    def initdata(self,data):
+        self.data=data
+    def initializeGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        self.camera=camera()
+        
+    def paintGL(self):
+        self.camera.Update()
+        self.drawbase()
+        #glutWireTeapot(0.5)
+        if self.data!=[]:
+            print("Load")
+            self.drawline(self.data,self.layerindex)
+            #self.VBO(self.data,0)
+        else:
+            print('mesh is none')
+    def drawbase(self):
+        glPushMatrix()
+        glBegin(GL_LINE_LOOP)
+        glColor(0,1,0)
+        glVertex3f(0,0,0)
+        glVertex3f(0,250,0)
+        glVertex3f(250,250,0)
+        glVertex3f(250,0,0)
+        glEnd()
+        glPopMatrix()
+    def drawline(self,list_usepoint,layer_num):
+        
+        for i in range(0,len(list_usepoint)):
+            
+            #print("i:",i)
+            point_num=len(list_usepoint[i][layer_num])
+            #print("多段线数量:",point_num)
+            for j in range(0,point_num):
+                glBegin(GL_LINE_LOOP)
+                glColor(1,0,0)
+                #print("第{0}个多段线".format(j))
+                for w in range(0,len(list_usepoint[i][layer_num][j])):
+                    #print("第{0}个点".format(w))
+                    glVertex3f(list_usepoint[i][layer_num][j][w][0],list_usepoint[i][layer_num][j][w][1],0)
+                    #print("({0},{1},{2})".format(list_usepoint[i][layer_num][j][w][0],list_usepoint[i][layer_num][j][w][1],z))
+                glEnd()
+        
+    def VBO(self,list_usepoint,layer_num):
+        #glPushMatrix()
+        data=array(list_usepoint[0][layer_num][0])
+        print(data)
+        print(len(data))
+        vb=vbo.VBO(data)
+        vb.bind()
+        #glEnableClientState(GL_NORMAL_ARRAY)
+        glEnableClientState(GL_VERTEX_ARRAY)
+        #glNormalPointer(GL_FLOAT, 24, vb+12)
+        glVertexPointer(3, GL_FLOAT, 12, vb )#在每一个周期里，都有6个32-bit浮点数据，总共4*6=24     3代表数量
+        glDrawArrays(GL_TRIANGLES, 0, len(data))
+        vb.unbind()
+        #glDisableClientState(GL_NORMAL_ARRAY)
+        glDisableClientState(GL_VERTEX_ARRAY)
+        #glPopMatrix()
+    def mousePressEvent(self,event):
+        self.myMousePosition=event.pos()
+        if event.button()==QtCore.Qt.LeftButton:
+            self.isMove=True
+        if event.button()==QtCore.Qt.RightButton:
+            self.isRotate=True
+        #self.update()
+    def wheelEvent(self,event):
+        if event.angleDelta().y()>0:
+            self.camera.right*=0.95
+            self.camera.top*=0.95
+        if event.angleDelta().y()<0:
+            self.camera.right/=0.95
+            self.camera.top/=0.95
+        self.update()
+    def mouseReleaseEvent(self,event):
+        if event.button()==QtCore.Qt.LeftButton:
+            self.isMove=False
+        if event.button()==QtCore.Qt.RightButton:
+            self.isRotate=False
+    def mouseMoveEvent(self,event):
+        if self.isMove:
+            moveX=event.pos().x()-self.myMousePosition.x()
+            moveY=event.pos().y()-self.myMousePosition.y()
+            self.camera.Move(moveX*self.camera.right/300,moveY*self.camera.right/300)
+            self.myMousePosition=event.pos()
+        if self.isRotate:
+            moveX=event.pos().x()-self.myMousePosition.x()
+            moveY=event.pos().y()-self.myMousePosition.y()
+            self.camera.Yaw(-moveX)
+            self.camera.Pitch(-moveY)
+            #self.light.lightpos=[-self.camera.forwardDir.X,-self.camera.forwardDir.Y,-self.camera.forwardDir.Z,0]
+            self.myMousePosition=event.pos()
+        self.update()
